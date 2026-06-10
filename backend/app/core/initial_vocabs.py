560781acf29d0e1880428e8390790b0ef9fa4554
smnclmns <simon2.clemens@tu-dortmund.de>
2026-06-10 12:24:26 +0200

    refactor: remove nmr specific vocabs from initial vocab

--- backend/app/core/initial_vocabs.py
+++ backend/app/core/initial_vocabs.py
@@ -23,12 +23,4 @@ INITIAL_VOCABS = [
         rdf_source=HttpUrl("https://qudt.org/vocab/constant"),
         identifier="http://qudt.org/vocab/constant",
     ),
-    InitialVocab(
-        rdf_source=HttpUrl("https://raw.githubusercontent.com/rsc-ontologies/rsc-cmo/master/chmo.owl"),
-        identifier="http://purl.obolibrary.org/obo/chmo.owl",
-    ),
-    InitialVocab(
-        rdf_source=HttpUrl("http://nmrML.org/nmrCV.owl"),
-        identifier="http://nmrML.org/nmrCV",
-    ),
 ]
